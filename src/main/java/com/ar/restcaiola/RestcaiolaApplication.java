dc68acb40d10fa2316324e8c88c798712ae0e991
Javier <javiteen1250@gmail.com>
2023-06-16 12:07:45 -0300

    primer commit

---
+++ src/main/java/com/ar/restcaiola/RestcaiolaApplication.java
@@ -0,0 +1,13 @@
+package com.ar.restcaiola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcaiolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcaiolaApplication.class, args);
+	}
+
+}
