fd1c9ccfa8b08b1eff6d94c881607dd8cd724100
Javier <javiteen1250@gmail.com>
2023-06-20 22:49:26 -0300

    iniciando la configuracion de spring security con el package principal

---
+++ src/main/java/com/ar/restcaiola/security/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.ar.restcaiola.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+	
+	SecurityFilterChain filterchain () {
+		
+	}
+
+}
